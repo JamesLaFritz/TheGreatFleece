40b5093a111c74ae4e463d7708d72bd5f3213c36
James LaFritz <ktmarine1999@gmail.com>
2021-05-29 17:13:19 -0400

    UI Manager Added and Hooked Up

--- Assets/The Great Fleece/Game/Scripts/Managers/UIManager.cs
+++ Assets/The Great Fleece/Game/Scripts/Managers/UIManager.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[DisallowMultipleComponent]
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static UIManager _instance;
+
+    public static UIManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("The Audio Manager is null!!!");
+            }
+
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _instance = this;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBGL
+        Application.OpenURL("https://JamesLaFritz.com");
+        #else
+        Application.Quit();
+        #endif
     }
 }
