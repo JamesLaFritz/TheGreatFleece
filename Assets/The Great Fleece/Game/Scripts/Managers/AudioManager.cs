73e253c276639725600e54088f6d35c05534012b
James LaFritz <ktmarine1999@gmail.com>
2021-05-29 10:02:38 -0400

    Audio Manager Added

---
+++ Assets/The Great Fleece/Game/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_voiceOverAudioSource;
+    private bool m_hasVoiceOverSource;
+    [SerializeField] private AudioSource m_backgroundAudioSource;
+    private bool m_hasBackgroundSource;
+    [SerializeField] private AudioClip m_defaultBackgroundMusic;
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("The Audio Manager is null!!!");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+
+        m_hasBackgroundSource = m_backgroundAudioSource != null;
+        m_hasVoiceOverSource = m_voiceOverAudioSource != null;
+    }
+
+    public void PlayVoiceOverClip(AudioClip clip)
+    {
+        if (!m_hasVoiceOverSource) return;
+        Debug.Assert(m_voiceOverAudioSource != null, nameof(m_voiceOverAudioSource) + " != null");
+        m_voiceOverAudioSource.PlayOneShot(clip);
+    }
+
+    public void PlayBackgroundMusic(AudioClip clip = null)
+    {
+        if (!m_hasBackgroundSource) return;
+
+        Debug.Assert(m_backgroundAudioSource != null, nameof(m_backgroundAudioSource) + " != null");
+
+        if (m_backgroundAudioSource.isPlaying)
+            m_backgroundAudioSource.Stop();
+
+        if (clip == null)
+            clip = m_defaultBackgroundMusic;
+        m_backgroundAudioSource.clip = clip;
+        m_backgroundAudioSource.loop = true;
+        m_backgroundAudioSource.Play();
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/Triggers/VoiceOverTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class VoiceOverTrigger : MonoBehaviour
+{
+    private bool m_hasBeenTriggered;
+
+    [SerializeField] [Tag] private string m_triggerTag = "Player";
+
+    private bool m_hasTags;
+
+    [SerializeField] AudioClip m_voiceOverClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{name} Colliding with {other.name} with tag {other.name}");
+
+        if (m_hasBeenTriggered) return;
+
+        Debug.Assert(other != null, nameof(other) + " != null");
+        if (!other.CompareTag(m_triggerTag)) return;
+
+        Debug.Assert(AudioManager.Instance != null, "AudioManager.Instance != null");
+        AudioManager.Instance.PlayVoiceOverClip(m_voiceOverClip);
+
+        m_hasBeenTriggered = true;
+    }
+}
